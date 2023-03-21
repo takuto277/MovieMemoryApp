d2b17d61d153250772e57163839440bfd11c627a
Takuto <Takuto.72.soccer@gmail.com>
2023-03-21 16:10:44 +0900

    Initial Commit

---
+++ MovieMemoryAppUITests/MovieMemoryAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MovieMemoryAppUITestsLaunchTests.swift
+//  MovieMemoryAppUITests
+//
+//  Created by 小野拓人 on 2023/03/21.
+//
+
+import XCTest
+
+final class MovieMemoryAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
