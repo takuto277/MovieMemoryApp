3ee4d4cbfc307bb0e5f5f4d42d4a437ad69914bd
Takuto <Takuto.72.soccer@gmail.com>
2023-03-24 00:34:57 +0900

    nibの接続

--- MovieMemoryApp/Modules/Home/ViewController/HomeViewController.swift
+++ MovieMemoryApp/Modules/Home/ViewController/HomeViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 final class HomeViewController: UIViewController {
     
-    @IBAction func button(_ sender: Any) {
-        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let movieDetailViewController = storyboard.instantiateViewController(withIdentifier: "MovieDetail") as! MovieDetailViewController
-        self.navigationController?.pushViewController(movieDetailViewController, animated: true)
+//    @IBOutlet var muView: UIView!
+//
+//    @IBAction func button(_ sender: Any) {
+//        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+//        let movieDetailViewController = storyboard.instantiateViewController(withIdentifier: "MovieDetail") as! MovieDetailViewController
+//        self.navigationController?.pushViewController(movieDetailViewController, animated: true)
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let nib = UINib(nibName: "HomeViewController", bundle: nil)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        self.view = view
     }
 }
