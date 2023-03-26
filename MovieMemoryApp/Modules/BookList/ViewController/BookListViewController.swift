e541e13ef3c3609f1ff230235b48a3f25b9be321
Takuto <Takuto.72.soccer@gmail.com>
2023-03-26 16:25:32 +0900

    BookListの実装

--- MovieMemoryApp/Modules/BookList/ViewController/BookListViewController.swift
+++ MovieMemoryApp/Modules/BookList/ViewController/BookListViewController.swift
@@ -9,7 +9,26 @@ import UIKit
 
 final class BookListViewController: UIViewController {
     
+    @IBOutlet weak var collection: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.collection.delegate = self
+        self.collection.dataSource = self
+        self.collection.register(UINib(nibName: String(describing: BookListCollectionViewCell.self), bundle: nil), forCellWithReuseIdentifier: "bookList")
     }
+}
+
+extension BookListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collection.dequeueReusableCell(withReuseIdentifier: "bookList", for: indexPath) as! BookListCollectionViewCell
+        cell.textLabel.text = String(indexPath.row)
+        return cell
+    }
+    
+    
 }
---
+++ MovieMemoryApp/Util/CustomCell/BookListCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  BookListCollectionViewCell.swift
+//  MovieMemoryApp
+//
+//  Created by 小野拓人 on 2023/03/26.
+//
+
+import UIKit
+
+class BookListCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var textLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
